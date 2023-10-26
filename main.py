7c6f2e480ac802ee422ee39a9f56be1fe1ffa46e
K.M.K.R Kariyapperuma <83937185+123kavi@users.noreply.github.com>
2023-10-26 13:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def upload_image(
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, FastAPI is working!"}
+    return {"message": "Hello, bvbv is working!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host='192.168.8.100', port=8000)
